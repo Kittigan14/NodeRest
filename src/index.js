a9297f2ad18c7d2b3b0cb969159f97613a96bd71
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-01-29 21:01:52 +0700

    Update Code

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.get("/" , (req,res) => {
-  res.send("../indexhome.html");
+  res.send("Hello World!");
 });
 
 app.listen(port , () => {
